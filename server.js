85502cc26a81f06c4cc6f98826b39b6a953f91ab
sundippatel <sundip.patel@outlook.com>
2015-05-11 12:50:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello World - Testing SAN-1485');
+    res.send('Hello World - Testing SAN-1485...');
 });
 
 app.listen(443);
